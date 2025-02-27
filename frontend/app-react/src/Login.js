d2906e501c11aaa695963e53f1386388994cf41c
Adrien Maestri <adrien.maestri@gmail.com>
2025-02-27 21:08:51 +0100

    Change button + link to board

--- frontend/app-react/src/Login.js
+++ frontend/app-react/src/Login.js
@@ -1,25 +1,36 @@
 import './style.css';
-import { Link,Routes,Route } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //TODO rajouter verification backend
+    navigate('/board'); 
+  };
+
   return (
     <>
-    {/*<Link to="/tableaubord">Tableau de bord</Link>*/}
+    {/*<Link to="/board">Tableau de bord</Link>*/}
     <div className='login'>
       <h1>Donne ta tune.net</h1>
       <div className='login-div'>
         <h2>Connexion</h2>
-        <form className='login-form'>
+        <form className='login-form' onSubmit={handleSubmit}>
           <div>
-            <input name='username' type='text' placeholder='Username'/>
+            <input name='username' type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)}/>
             <label className='login-label' htmlFor='username'>Username</label>
           </div>
           
           <div>
-            <input name='password' type='password' placeholder='Mot de passe'/>
+            <input name='password' type='password' placeholder='Mot de passe' value={password} onChange={(e) => setPassword(e.target.value)}/>
             <label className='login-label' htmlFor='password'>Mot de passe</label>
           </div>
-          <button>
+          <button className='login-button'>
             Se connecter
           </button>
         </form>
